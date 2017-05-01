152756af8f776077bbe2a95c41889ab2a10c3433
nguyendhoang <hoang.nguyen@saleshood.com>
2017-05-01 18:05:40 +0700

    Fix UsersController#show

--- MobileApi/config/routes.rb
+++ MobileApi/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     scope module: :v1, constraints: ApiConstraint.new(version: :v1) do
-    	get 'home', to: 'home#index'
-    	resources :users, only: [:create, :update, :show]
-      resources :notes, only: [:index, :create, :show, :update, :destroy] 
+      get 'home', to: 'home#index'
+      resources :users, only: [:create, :update, :show]
+      resources :notes, only: [:index, :create, :show, :update, :destroy]
     end
   end
 
